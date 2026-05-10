bc4ac91acb9cdace461564ffefc4dc5fe17ca762
Soura <martisoura@gmail.com>
2026-05-10 17:07:39 +0530

    fix: remove unused StatusBadge import from PayAnalysisSection

--- dashboard/frontend/src/components/sections/PayAnalysisSection.jsx
+++ dashboard/frontend/src/components/sections/PayAnalysisSection.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useOverviewData } from '../../hooks/useOverviewData'
 import { ToneChip } from '../primitives/ToneChip'
-import { StatusBadge } from '../primitives/StatusBadge'
 import { EvidenceDrawer } from '../shared/EvidenceDrawer'
 
 const REVIEW_STATE_LABELS = {
